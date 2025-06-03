3bffb1e797f3628e7baf9591a0b49133c3b401a9
ZER0-GIT <lol.bicio@gmail.com>
2025-06-03 01:47:30 -0500

    Initial commit

---
+++ src/main/java/controller/Login.java
@@ -0,0 +1,28 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private Label lblContrasena;
+
+    @FXML
+    private TextField txtContrasena;
+
+    @FXML
+    private TextField txtUsuario;
+
+}
---
+++ src/main/java/utils/Paths.java
@@ -0,0 +1,5 @@
+package utilities;
+
+public class Paths {
+    public static final String LoginFXML = "/view/Login.fxml";
+}
