fbb240abbc0c069022c8608ea5d4808bd3e17384
ZER0-GIT <lol.bicio@gmail.com>
2025-07-17 20:52:40 -0500

    Cambios para el controlador de horas: - Arreglo del error de formato de hora ingresada en PararA - Añadido automatico de cantidad de pc's - Nuevo util (config)

--- src/main/java/controller/Controlador.java
+++ src/main/java/controller/Controlador.java
@@ -5,13 +5,19 @@ import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javax.swing.JOptionPane;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Duration;
 import javafx.util.converter.DefaultStringConverter;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import model.Cabina;
+import utils.Config;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -34,7 +40,7 @@ public class Controlador implements Initializable {
         tablaCabinas.setEditable(true);
 
         colNumero.setCellValueFactory(cellData -> cellData.getValue().numeroProperty().asObject());
-        colInicio.setCellValueFactory(cellData -> cellData.getValue().inicioProperty());
+        colInicio.setCellValueFactory(cellData -> cellData.getValue().inicioProperty()); 
         colContador.setCellValueFactory(cellData -> cellData.getValue().contadorProperty());
         colEstado.setCellValueFactory(cellData -> cellData.getValue().estadoProperty());
         colMensaje.setCellValueFactory(cellData -> cellData.getValue().mensajeProperty());
@@ -44,22 +50,33 @@ public class Controlador implements Initializable {
 
         colParaA.setOnEditCommit(event -> {
             Cabina cabina = event.getRowValue();
+            LocalTime horaInicio=LocalTime.now();
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+            cabina.setInicio(horaInicio.format(formato));
             String nuevoParaA = event.getNewValue().trim();
-            cabina.setParaA(nuevoParaA);
-
+            try {
+                LocalTime hora = LocalTime.parse(nuevoParaA);
+                nuevoParaA = hora.format(formato);
+                cabina.setParaA(nuevoParaA);
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato de hora inválido: " + nuevoParaA);
+                cabina.setParaA("00:00:00");
+            }
             int segundos = calcularDiferenciaEnSegundos(cabina.getInicio(), nuevoParaA);
             cabina.setContadorSegundos(segundos);
 
             if (segundos > 0) {
                 cabina.setEstado("Activo");
             } else {
+                JOptionPane.showMessageDialog(null, "Hora acabada", "La hora de la PC X", JOptionPane.INFORMATION_MESSAGE);
                 cabina.setEstado("Desconectado");
             }
         });
 
         listaCabinas = FXCollections.observableArrayList();
-        listaCabinas.add(new Cabina(1, "10:00", 15 * 60, "Activo", "10:15", "Cliente frecuente"));
-
+        for (int i = 1; i <= Config.cantPc; i++) {
+            listaCabinas.add(new Cabina(i,"",0,"Desconectado","",""));
+        }
         tablaCabinas.setItems(listaCabinas);
 
         timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
@@ -80,15 +97,18 @@ public class Controlador implements Initializable {
         try {
             String[] ini = inicio.trim().split(":");
             String[] fin = paraA.trim().split(":");
-            if (ini.length == 2 && fin.length == 2) {
+
+            if (ini.length == 3 && fin.length == 3) {
                 int iniHoras = Integer.parseInt(ini[0]);
                 int iniMin = Integer.parseInt(ini[1]);
+                int iniSeg = Integer.parseInt(ini[2]);
 
                 int finHoras = Integer.parseInt(fin[0]);
                 int finMin = Integer.parseInt(fin[1]);
+                int finSeg = Integer.parseInt(fin[2]);
 
-                int iniTotal = iniHoras * 3600 + iniMin * 60;
-                int finTotal = finHoras * 3600 + finMin * 60;
+                int iniTotal = iniHoras * 3600 + iniMin * 60 + iniSeg;
+                int finTotal = finHoras * 3600 + finMin * 60 + finSeg;
 
                 return Math.max(0, finTotal - iniTotal);
             }
@@ -97,4 +117,5 @@ public class Controlador implements Initializable {
         }
         return 0;
     }
+
 }
---
+++ src/main/java/utils/Config.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Config {
+    public static final int cantPc = 20;
+    //Precio por minuto en centavos (ej: 1 min = 0.0167 y eso en una hora redondeada sería 1 sol)
+    public static final double precioMin = 0.0167;
+}
